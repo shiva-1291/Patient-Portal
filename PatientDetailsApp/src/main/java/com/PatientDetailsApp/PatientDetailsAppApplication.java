8d97c83e27bfb0abab9f6f4beb7c6878a9fffebd
Shivasaiteja <shivasaiteja12@gmail.com>
2020-04-30 20:57:41 -0400

    First Commit
    Uploading all the files

--- PatientDetailsApp/src/main/java/com/PatientDetailsApp/PatientDetailsAppApplication.java
+++ PatientDetailsApp/src/main/java/com/PatientDetailsApp/PatientDetailsAppApplication.java
@@ -3,7 +3,7 @@ package com.PatientDetailsApp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-
+//main starting app
 @SpringBootApplication
 public class PatientDetailsAppApplication {
 	public static void main(String[] args) {
